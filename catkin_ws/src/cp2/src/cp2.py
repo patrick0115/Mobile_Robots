6736dc37858e86c78459ccb6fef32ef3cded276f
patrick0115 <karta1770900@gmail.com>
2021-10-26 03:56:31 +0800

    add

--- catkin_ws/src/cp2/src/cp2.py
+++ catkin_ws/src/cp2/src/cp2.py
@@ -4,21 +4,21 @@ from std_msgs.msg import Int16
 
 def main():
     rospy.init_node('cp2')
-    pub_r = rospy.Publisher("right_pwm", Int16, queue_size = 10)
-    pub_l = rospy.Publisher("left_pwm", Int16, queue_size = 10)
+    pub_R = rospy.Publisher("pwm_value_R", Int16, queue_size = 10)
+    pub_L = rospy.Publisher("pwm_value_L", Int16, queue_size = 10)
     while not rospy.is_shutdown():
         try:
-            pwm_r = int(raw_input("user's right:"))
+            input_value_R= int(raw_input("user's right:"))
         except ValueError:
             rospy.logerr("Invalid input, set default value 0")
-            pwm_r = 0
+            intput_value_R = 0
         try:
-            pwm_l = int(raw_input("user's left:"))
+            input_value_L = int(raw_input("user's left:"))
         except ValueError:
             rospy.logerr("Invalid input, set default value 0")
-            pwm_l = 0
-        pub_r.publish(Int16(pwm_r))
-        pub_l.publish(Int16(pwm_l))
-        rospy.sleep(6.5)
+            intput_value_L = 0
+        pub_R.publish(Int16(intput_value_R))
+        pub_L.publish(Int16(intput_value_L))
+        rospy.sleep(3)
 if __name__=="__main__":
     main()
