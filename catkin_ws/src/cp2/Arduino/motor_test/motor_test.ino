0411bfa791ea4463d81f278606196d9cc1c81cd4
patrick0115 <34533575+patrick0115@users.noreply.github.com>
2021-10-28 19:10:38 +0800

    Create motor_test.ino

---
+++ catkin_ws/src/cp2/Arduino/motor_test/motor_test.ino
@@ -0,0 +1,29 @@
+#define in1  4
+#define in2 5
+#define in3 6
+#define in4 7
+#define ENA 10
+#define ENB 11
+
+void setup() {
+  pinMode(OUTPUT, in1);
+  pinMode(OUTPUT, in2);
+  pinMode(OUTPUT, in3);
+  pinMode(OUTPUT, in4);
+  pinMode(OUTPUT, ENA);
+  pinMode(OUTPUT, ENB);
+
+}
+
+void loop() {
+  f();
+
+}
+void f(){
+    digitalWrite(in1,HIGH);
+    digitalWrite(in2,LOW);
+    digitalWrite(in3,HIGH);
+    digitalWrite(in4,LOW);
+    analogWrite(ENA,70);
+    analogWrite(ENB,60);    
+}
