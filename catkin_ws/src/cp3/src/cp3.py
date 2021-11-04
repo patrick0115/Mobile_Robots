86d5a326b716dc2e4170b0b2ece2444d28dd3916
patrick0115 <34533575+patrick0115@users.noreply.github.com>
2021-11-04 16:15:13 +0800

    Update cp3.py

--- catkin_ws/src/cp3/src/cp3.py
+++ catkin_ws/src/cp3/src/cp3.py
@@ -6,7 +6,7 @@ from std_msgs.msg import Bool
 from std_msgs.msg import Int16
 from time import sleep
 
-LIGHT_PIN = 4 
+touch_pin = 4 
 
 def main():
     rospy.init_node('cp3')
@@ -14,10 +14,10 @@ def main():
     pub = rospy.Publisher('cp3_topic', Bool, queue_size = 10)
 
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(LIGHT_PIN, GPIO.IN)
+    GPIO.setup(touch_pin , GPIO.IN, pull_up_down=GPIO.PUD_UP)
     while not rospy.is_shutdown():
         while True:
-             print GPIO.input(LIGHT_PIN)
+             print GPIO.input(touch_pin)
              print('--------------')
              rospy.sleep(0.5)
 
