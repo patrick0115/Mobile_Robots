77f478dc3ce769ca4e4ff764639c32843a1cb299
patrick <karta1770900@gmail.com>
2021-12-03 00:34:16 +0800

    dd

--- catkin_ws/src/cp4/src/cp4.py
+++ catkin_ws/src/cp4/src/cp4.py
@@ -9,8 +9,11 @@ touch_R = 14
 touch_B = 15
 
 def main():
-    rospy.init_node('cp3')
+    rospy.init_node('cp4')
     pub = rospy.Publisher('touch', Int16, queue_size = 10)
+ #   rospy.Subscriber("light_value", Int16, cb_light , queue_size=1)
+    rospy.Subscriber("ir_value", Int16, cb_ir , queue_size=1)
+
     state=4 #forward
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(touch_L , GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -20,27 +23,40 @@ def main():
     while not rospy.is_shutdown():
         if(GPIO.input(touch_B)==0):
             state=0
-            print(GPIO.input(touch_R))
+            print("buttom")
+            pub.publish(Int16(state))
             rospy.sleep(5)
         elif(GPIO.input(touch_L)==1 and GPIO.input(touch_R)==0 and GPIO.input(touch_B)==1):
             state=1 #turn left
-            print(GPIO.input(touch_R))
+            print("turn left")
+            pub.publish(Int16(state))
             rospy.sleep(5)
         elif(GPIO.input(touch_L)==0 and GPIO.input(touch_R)==1 and GPIO.input(touch_B)==1):
             state=2 #turn right
-            print(GPIO.input(touch_L))
+            print("turn right")
+            pub.publish(Int16(state))
             rospy.sleep(5)
         elif(GPIO.input(touch_L)==0 and GPIO.input(touch_R)==0 and GPIO.input(touch_B)==1):
             state=3 #back
+            print("go back")
+            pub.publish(Int16(state))
             rospy.sleep(5)
         else:
             state=4 #forward
-        pub.publish(Int16(state))
+            print("go straight")
+            pub.publish(4)
+            rospy.sleep(3)
+        rospy.sleep(0.1)
+   #     rospy.spin()        
 
-        print(state)
-        print('--------------')
-        rospy.sleep(0.5)
-        
+
+def cb_ir(msg_i):
+    rospy.loginfo(rospy.get_caller_id() + "ir value is %d", msg_i.data)
+
+
+
+def cb_light(msg_l):
+    rospy.loginfo(rospy.get_caller_id() + "light value is %d", msg_l.data)
 
 if __name__ == '__main__':
     main()       
