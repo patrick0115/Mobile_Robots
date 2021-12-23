eaa784feab5f4c56bbaee0b62fb5d6722d699e67
patrick0115 <karta1770900@gmail.com>
2021-12-24 02:36:41 +0800

    add

--- catkin_ws/src/cp2/src/cp2.py
+++ catkin_ws/src/cp2/src/cp2.py
@@ -5,23 +5,15 @@ from std_msgs.msg import Int16
 
 def main():
     rospy.init_node('cp2')
-    pub_R = rospy.Publisher("pwm_value_R", Int16, queue_size = 10)
-    pub_L = rospy.Publisher("pwm_value_L", Int16, queue_size = 10)
+    pub = rospy.Publisher("dir_value", Int16, queue_size = 10)
     while not rospy.is_shutdown():
         rospy.sleep(3)
         try:
-            input_value_R= int(raw_input("user's right:"))
-            pub_R.publish(Int16(input_value_R))
+            dir_value= int(raw_input("user's right:"))
+            pub.publish(Int16(dir_value))
         except ValueError:
             rospy.logerr("Invalid input, set default value 0")
             intput_value_R = 0
-        try:
-            input_value_L = int(raw_input("user's left:"))
-            pub_L.publish(Int16(input_value_L))
-        except ValueError:
-            rospy.logerr("Invalid input, set default value 0")
-            intput_value_L = 0
-
         rospy.sleep(3)
 if __name__=="__main__":
     main()
